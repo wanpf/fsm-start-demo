25a16a8f2e62c5d61c7ec9e10dc2643c13f272c0
Cybwan <baili@flomesh.io>
2022-09-06 11:28:53 +0800

    Update scripts.

--- scripts/deploy-udp-echo-demo.sh
+++ scripts/deploy-udp-echo-demo.sh
@@ -9,6 +9,8 @@ export osm_namespace=osm-system
 kubectl delete namespace udp-demo --ignore-not-found
 kubectl create namespace udp-demo
 
+$OSM/bin/osm namespace --mesh-name "osm" add udp-demo
+
 kubectl apply -f demo/udp-echo/udp-echo-rbac.yaml -n udp-demo
 kubectl apply -f demo/udp-echo/udp-echo-service.yaml -n udp-demo
 kubectl apply -f demo/udp-echo/udp-echo-deployment.yaml -n udp-demo
@@ -16,11 +18,9 @@ kubectl apply -f demo/udp-echo/udp-echo-deployment.yaml -n udp-demo
 kubectl apply -f demo/udp-echo/udp-client-rbac.yaml -n udp-demo
 kubectl apply -f demo/udp-echo/udp-client-deployment.yaml -n udp-demo
 
-$OSM/bin/osm namespace add udp-demo
+sleep 10
 
 kubectl wait --for=condition=ready pod -n udp-demo -l app=udp-echo --timeout=180s
-
 kubectl wait --for=condition=ready pod -n udp-demo -l app=udp-client --timeout=180s
 
-sleep 5
 kubectl logs "$(kubectl get pod -n udp-demo -l app=udp-echo -o jsonpath='{.items..metadata.name}')" -n udp-demo -c udp-echo-server -f
