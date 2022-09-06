f1adf36c077e147695f074eeecc548cf81d507d6
Cybwan <baili@flomesh.io>
2022-09-06 15:12:02 +0800

    Update UDPRoute demo.

--- scripts/deploy-udp-echo-demo.sh
+++ scripts/deploy-udp-echo-demo.sh
@@ -13,14 +13,21 @@ $OSM/bin/osm namespace --mesh-name "osm" add udp-demo
 
 kubectl apply -f demo/udp-echo/udp-echo-rbac.yaml -n udp-demo
 kubectl apply -f demo/udp-echo/udp-echo-service.yaml -n udp-demo
-kubectl apply -f demo/udp-echo/udp-echo-deployment.yaml -n udp-demo
+
+kubectl apply -f demo/udp-echo/udp-echo-service-v1.yaml -n udp-demo
+kubectl apply -f demo/udp-echo/udp-echo-deployment-v1.yaml -n udp-demo
+
+kubectl apply -f demo/udp-echo/udp-echo-service-v2.yaml -n udp-demo
+kubectl apply -f demo/udp-echo/udp-echo-deployment-v2.yaml -n udp-demo
 
 kubectl apply -f demo/udp-echo/udp-client-rbac.yaml -n udp-demo
 kubectl apply -f demo/udp-echo/udp-client-deployment.yaml -n udp-demo
+
+kubectl apply -f demo/udp-echo/udp-smi-policy.yaml -n udp-demo
 
 sleep 5
 
 kubectl wait --for=condition=ready pod -n udp-demo -l app=udp-echo --timeout=180s
 kubectl wait --for=condition=ready pod -n udp-demo -l app=udp-client --timeout=180s
 
-kubectl logs "$(kubectl get pod -n udp-demo -l app=udp-echo -o jsonpath='{.items..metadata.name}')" -n udp-demo -c udp-echo-server -f
+kubectl logs "$(kubectl get pod -n udp-demo -l app=udp-echo-v1 -o jsonpath='{.items..metadata.name}')" -n udp-demo -c udp-echo-server -f
